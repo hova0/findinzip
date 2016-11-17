17609062c5b0f433383640c1ff4d5baaa9831ab6
Jared <hova__0@ghotmail.com>
2016-11-17 08:47:40 -0600

    wow its old

--- findinzip/Program.cs
+++ findinzip/Program.cs
@@ -12,20 +12,20 @@ namespace findinzip
         {
             if (args == null || args.Length == 0)
             {
-                Console.WriteLine("findinzip <filename.zip> <file_mask>");
+                Console.WriteLine("findinzip <filename.zip> <file_mask> -text <string>");
                 return 1;
             }
             try
             {
                 if (args.Length > 0 && !System.IO.File.Exists(args[0]))
                 {
-                    Console.WriteLine("Zip file does not exist");
+                    Console.WriteLine("Zip file does not exist or access denied");
                     return 1;
                 }
             }
-            catch (Exception exz)
+            catch (Exception)
             {
-                Console.WriteLine("Invalid filename");
+                Console.WriteLine("Invalid filename or access denied.");
                 return 1;
             }
 
@@ -33,4 +33,46 @@ namespace findinzip
             return 0;
         }
     }
+
+    public class Unzipper : IDisposable
+    {
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Unzipper() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+
+
 }
